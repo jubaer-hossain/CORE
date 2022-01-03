2cf497eb4d4474497ec10f5dc79fadebb44ec7d2
Jubaer <hello@MacBooks-MacBook.local>
2022-01-03 08:13:15 +0600

    Small fixes

--- Right Sibling Tree.py
+++ Right Sibling Tree.py
@@ -1,3 +1,11 @@
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+    def __init__(self, value, left = None, right = None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
 """
 Sudocode: 
 1. If no parent then right is None
